4b2d9dfbd54f545b62f4e1ecae6ab957fe06c5b4
Sufiyan <csesufi@gmail.com>
2025-10-07 15:35:43 +0530

    mongodb connected successfully

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 import express from "express";
 import "dotenv/config.js";
 import cors from "cors";
+import connectDB from "./config/db.js";
+import adminRouter from "./routes/adminRoutes.js";
 
 const app = express();
+
+await connectDB();
 
 //Middleware
 app.use(cors());
@@ -10,6 +14,8 @@ app.use(express.json()); //all the request pass using the json
 
 //Routes
 app.get("/", (req, res) => res.send("api is working")); //home route
+
+app.use("/api/admin", adminRouter);
 
 const PORT = process.env.PORT || 3000;
 
