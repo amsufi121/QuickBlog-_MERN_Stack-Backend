158d03e3f623145c827b222c17f5c72a08b2ed43
Sufiyan <csesufi@gmail.com>
2025-10-08 15:51:25 +0530

    created api for admin dashboard

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -79,6 +79,10 @@ export const deleteBlogById = async (req, res) => {
   try {
     const { id } = req.parse;
     await Blog.findByIdAndDelete(id);
+
+    //delete all comments associated with the blog4
+    await Comment.deleteMany({ blog: id });
+
     res.json({ success: true, message: "Blog deleted successfully" });
   } catch (error) {
     res.json({ success: false, message: error.message });
